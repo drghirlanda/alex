8bdec4c577cd6234a826a3eb544dec5e97e2009e
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-03-23 16:14:05 -0400

    Added Manual pandoc template in case we want to change formatting.

--- Docs/make_manual.sh
+++ Docs/make_manual.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
-pandoc -V geometry="margin=1in" -V fontsize=12pt -s --toc Manual.md -o Manual.pdf
+pandoc --template Manual.tpl -V geometry="margin=1in" -V fontsize=12pt -s --toc Manual.md -o Manual.pdf
 pandoc -s Manual.md -o Manual.html
