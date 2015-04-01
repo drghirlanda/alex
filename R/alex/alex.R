eca0f42f50464b4fdc5ca59cf918f14b8982b73a
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-04-01 17:30:19 -0400

    Started a bit of work on R package

--- R/alex/alex.R
+++ R/alex/alex.R
@@ -1,55 +1,44 @@
 # a function to build a 'presentation' variable that counts the number
 # of times any given stimulus has been presented, for each phase of
 # the experiment.
-alex.presentation <- function( data,
-                              subject="subject",
-                              cs="CS",
-                              phase="phase" ) {
-  pres <- rep( NA, nrow(data) )
+alex.presentation <- function( data ) {
+    pres <- rep( NA, nrow(data) )
+    
+    n.subjects <- max( data$Subject )
+    phases <- unique( data$Phase )
 
-  subject1 <- try( min( data[[subject]] ), silent=TRUE )
-  if( class(subject1) == "try-error" | !is.finite(subject1) ) {
-    stop( paste("no", subject, "variable found" ) )
-  }
-  n.subjects <- length( unique( data[[subject]] ) )
-  n.trials <- nrow(data) / n.subjects
-  print( paste( "Subjects:", n.subjects ) )
-  print( paste( "Trials:", n.trials ) )
-  
-  phases <- unique( data[[phase]] )
-  print( paste( "Found phases:", paste(phases, collapse=" " ) ) )
-
-  for( p in phases ) {
-    print( paste( "Analyzing phase", p ) )
-    my.phase <- data[[phase]] == p
-    stimuli <- unique( data[[cs]][ my.phase ])
-    print( paste( "Found stimuli:", paste(stimuli, collapse=" " ) ) )
-
-    for( s in stimuli ) {
-      my.cs <- data[[cs]] == s
-      n.pres <- sum( my.phase & my.cs & data[[subject]]==subject1 )
-      print( paste( "Found", n.pres, "presentations of", s ) )
-      pres[ my.phase & my.cs ] <- 1:n.pres
+    for( p in phases ) {
+        my.phase <- subset( data, Phase==p )
+        my.cs <- with( subset( data, my.phase, Stimulus )
+        for( s in stimuli ) {
+            trials <- with( subset( data, my.phase & my.cs ), Trial )
+            count <- 1
+            for( t in unique(trials) ) {
+                cases <- my.phase & my.cs & data$Trial == t 
+                pres[ cases ] <- count
+                count <- count + 1
+            }
+        }
     }
-  }
 
-  pres
+    pres
 }
 
 # a function to read data output by our PEBL programs
-read.alex.data <- function( data.dir="." ) {
-  data <- NULL
-  filenames <- dir( paste(data.dir,"/Data",sep=""), full.names=TRUE)
-  for( i in grep( "\\.dat$", filenames ) ) {
-    subject.data <- read.table( filenames[i], head=TRUE )
-    data <- rbind( data, subject.data )
-  }
+alex.read <- function( data.dir="." ) {
+    data <- NULL
+    filenames <- dir( paste(data.dir,"/Data",sep=""), full.names=TRUE)
+    for( i in grep( "\\.dat$", filenames ) ) {
+        subject.data <- read.table( filenames[i], head=TRUE )
+        data <- rbind( data, subject.data )
+    }
   data <- data.frame( data )
   ## make phase an unordered factor
   data$Phase <- factor( data$Phase, ordered=FALSE )
   ## correct sex factor (R reads F as "FALSE")
   data$Sex[ data$Sex != "M" ] <- "F"
   data$Sex <- factor( data$Sex, ordered=FALSE )
+  data$Presentation <- alex.presentation( data )
   data
 }
 
