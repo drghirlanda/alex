c930f09dbbcf719bba34789b808f86d819166a05
mdarkcity <mdarkcity@gmail.com>
2015-05-06 13:32:53 -0400

    Removed sed from make_manual

--- make_manual.sh
+++ make_manual.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # we use sed to replace longtable with table... 
 cd Docs
-pandoc --template AlexManual.tpl -V geometry="margin=1in" -V fontsize=11pt -s --toc AlexManual.md -t latex -o - | sed -re 's/\\begin\{longtable\}\[c\]/\\begin\{table\*\}\[t\]\\begin\{center\}\\small\\begin\{tabular\}/g' | sed -re 's/\\end\{longtable\}/\\end\{center\}\\end\{table\*\}/g' | sed -re 's/\\caption\{/\\end\{tabular\}\\caption\{/g' > AlexManual.tex
+pandoc --template AlexManual.tpl -V geometry="margin=1in" -V fontsize=11pt -s --toc AlexManual.md -t latex -o - > AlexManual.tex
 pdflatex AlexManual.tex
 pdflatex AlexManual.tex
 pdflatex AlexManual.tex
