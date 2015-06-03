609d4b8d43d368923b39f49c03008114bc5aac01
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-06-03 10:03:43 -0400

    Some exploratory work on the R code

--- R/alex/alex.R
+++ R/alex/alex.R
@@ -1,7 +1,7 @@
 # a function to build a 'presentation' variable that counts the number
 # of times any given stimulus has been presented, for each phase of
 # the experiment.
-alex.presentation <- function( data ) {
+presentation <- function( data ) {
     pres <- rep( NA, nrow(data) )
     n.subjects <- max( data$Subject )
     phases <- unique( data$Phase )
@@ -22,7 +22,7 @@ alex.presentation <- function( data ) {
 }
 
 # a function to read data output by our PEBL programs
-alex.read <- function( data.dir="." ) {
+read.alex <- function( data.dir="." ) {
     data <- NULL
     filenames <- dir( paste(data.dir,"/Data",sep=""), full.names=TRUE)
     for( i in grep( "\\.dat$", filenames ) ) {
@@ -37,23 +37,35 @@ alex.read <- function( data.dir="." ) {
     data$Sex[ data$Sex != "M" ] <- "F"
     data$Sex <- factor( data$Sex, ordered=FALSE )
     ## add presentation variable
-    data$Presentation <- alex.presentation( data )
+    data$Presentation <- alex::presentation( data )
     data
 }
 
-alex.hist <- function( data, variable, selection,
-                      xlabel=selection,
-                      xmin=0,
-                      xmax=max(data[[variable]], na.rm=TRUE) ) {
+hist <- function( data, variable, selection,
+                 xlabel=selection,
+                 xmin=0,
+                 xmax=max(data[[variable]], na.rm=TRUE) ) {
     x <- strsplit( selection, ":", fixed=TRUE )[[1]]
     my.phase <- x[1]
     my.stimulus <- x[2]
     my.count <- x[3]
     my.data <- subset( data, Phase == my.phase &
-                          Stimulus == my.stimulus )
+                      Stimulus == my.stimulus )
     if( my.count != "*" ) {
-        my.data <- subset( my.data, ResponseCount == my.count )
+      my.data <- subset( my.data, ResponseCount == my.count )
     }
     hist( my.data[[variable]], xlab=xlabel, main=NA,
          xlim=c(xmin, xmax) )
+}
+
+
+## this function builds a description of the design
+design <- function( data ) {
+  design <- NULL
+  phases <- unique( data$Phases )
+  for( p in phases ) {
+    design$Phases <- list()
+    design$Phases[[p]]$Stimuli <- unique( data$Stimulus )
+  }
+  design
 }
