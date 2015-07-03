59c732d2b16c74a71f6059f8fe50aa6e42bb9ecb
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-07-03 13:53:05 -0400

    Updated R files to properly build Presentation variable

--- R/alex/R/alex.R
+++ R/alex/R/alex.R
@@ -4,22 +4,30 @@
 presentation.and.keynum <- function( data ) {
   pres <- rep( NA, nrow(data) )
   keynum <- rep( NA, nrow(data) )
-  n.subjects <- max( data$Subject )
   phases <- unique( data$Phase )
-  for( p in phases ) {
-    my.phase <- data$Phase == p
-    for( s in unique( data$S1 ) ) {
-      my.stimulus <- data$S1 == s
-      my.trials <- data$Trial[ my.phase & my.stimulus ]
-      count <- 1
-      for( t in unique(my.trials) ) {
-        cases <- my.phase & my.stimulus & data$Trial == t 
-        pres[ cases ] <- count
-        keynum[ cases ] <- seq( 1, sum(cases), 1 )
-        count <- count + 1
+  gsubjects <- unique( data$GSubject )
+  for( gs in gsubjects ) {
+    my.gsubject <- data$GSubject == gs
+    for( p in phases ) {
+      my.phase <- data$Phase == p
+      for( s in unique( data$S1 ) ) {
+        my.stimulus <- data$S1 == s
+        my.trials <- data$Trial[ my.phase & my.stimulus ]
+        count <- 1
+        for( t in sort( unique(my.trials) ) ) {
+          cases <- my.gsubject & my.phase & my.stimulus & data$Trial==t 
+          r <- sum(cases)
+          if( r ) {
+            pres[ cases ] <- count
+            keynum[ cases ] <- seq( 1, r, 1 )
+            count <- count + 1
+          }
+        }
       }
     }
   }
+  keynum[ data$Key=="<timeout>" ] <- 0
+#  print( rep("-", 10) )
   list( Presentation=pres, KeyNum=keynum )
 }
 
@@ -33,6 +41,8 @@ read.alex <- function( data.dir="." ) {
         data <- rbind( data, subject.data )
     }
     data <- data.frame( data )
+    ## add global subject identifier
+    data$GSubject <- with( data, paste( Group, Subject, sep="." ) )
     ## make phase an unordered factor
     data$Phase <- factor( data$Phase, ordered=FALSE )
     ## correct sex factor (R reads F as "FALSE")
