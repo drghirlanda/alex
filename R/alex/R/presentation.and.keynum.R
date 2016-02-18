6e10ec9c11d22d4dc63650c9b61a96a757825395
Stefano Ghirlanda <drghirlanda@gmail.com>
2016-02-18 16:40:05 -0500

    Switching to data.table for R package. Faster read.alex.

--- R/alex/R/presentation.and.keynum.R
+++ R/alex/R/presentation.and.keynum.R
@@ -2,58 +2,22 @@
 #' first is the trial number for a given stimulus and phase, the
 #' second numbers responses within presentations.
 #'
-#' @param data An alex data.frame
+#' @param data An alex data.table
 #' @return A list with Presentation and KeyNum members
 #' @examples
 #' This function is meant for internal use
-presentation.and.keynum <- function( data ) {
-  ## create pres and keynum vectors:
-  pres <- rep( NA, nrow(data) )
-  keynum <- rep( NA, nrow(data) )
-  ## get information about phases and subjects:
-  phases <- unique( data$Phase )
-  gsubjects <- unique( data$GSubject )
-  ## loop through all phases, because presentations are calculated
-  ## within each phase:
-  for( p in phases ) {
-    ## selector for this phase:
-    my.phase <- data$Phase == p
-    ## loop through all stimuli, because presentaions are
-    ## stimulus-specific:
-    for( s in unique( data$S1 ) ) {
-      ## selector for this stimulus:
-      my.stimulus <- data$S1 == s
-      ## trials for this stimulus and this phase: 
-      my.trials <- data$Trial[ my.phase & my.stimulus ]
-      ## presentation counter:
-      count <- 1
-      ## loop through all trials. we cannot use simply 1:max(
-      ## my.trials ) because this s1 is not, generally, presented on
-      ## all trials:
-      for( t in sort( unique(my.trials) ) ) { 
-        ## data line selector for this trial:
-        cases <- my.phase & my.stimulus & data$Trial==t
-        ## number of data lines
-        r <- sum(cases) 
-        ## if found data lines:
-        if( r ) {
-          ## all these data lines are for the same presentation:
-          pres[ cases ] <- count
-          ## but they are keynums 1 through r:
-          keynum[ cases ] <- seq( 1, r, 1 )
-          ## increment presentation counter:
-          count <- count + 1
-        }
-      }
+presentation.and.keynum <- function( dt ) {
+  require(data.table)
+  for( p in unique(dt$Phase) ) {
+    for( s in unique(dt[ Phase==p, S1 ]) ) {
+      dt[ Phase==p & S1==s, Presentation := .GRP, by=Trial ]
+      dt[ Phase==p & S1==s, KeyNum := 1:.N, by=Trial ]
     }
   }
-  ## the above calculations for keynum are incorrect for <timeout>
-  ## trials. here the subject never responded, hence we set keynum=0:
-  keynum[ data$Key=="<timeout>" ] <- 0
+  dt[ Key=="<timeout>", KeyNum:=0 ]
   ## the following assigns to each ITI presentations the same number
   ## as the following stimulus. it relies on the fact that each trial
   ## is preceeded by an ITIs:
-  iti <- which( data$S1 == "ITI" )
-  pres[ iti ] <- pres[ iti+1 ]
-  list( Presentation=pres, KeyNum=keynum )
+  i <- which( dt$S1=="ITI" )
+  dt[ i, Presentation := dt$Presentation[ i+1 ] ]
 }
--- R/alex/R/read.alex.R
+++ R/alex/R/read.alex.R
@@ -1,38 +1,42 @@
 #' Read alex data residing in a Data directory under \code{data.dir}. Presentation
-#' and a KeyNum variables are added. The first is the number of the current presentation
+#' and KeyNum variables are added. The first is the number of the current presentation
 #' for the current stimulus (as opposed to Trial, which spans across stimuli). The second is
 #' the number of the current response in the current trial. See the alex manual (look for
 #' AlexManual.pdf in your alex installation, or use the command alex-manual, or look online
 #' at http://github.com/drghirlanda/alex).
 #'
 #' @param data.dir The directory where Data is found (default: the working directory)
-#' @return a data frame containing all read data
+#' @return a data table containing all read data
 #' @examples
 #' my.data <- read.alex()
 #' my.data <- read.alex( "Some/Directory" )
 #' @export
 read.alex <- function( data.dir=getwd() ) {
-    data <- NULL
-    filenames <- dir( paste(data.dir,"/Data",sep=""), full.names=TRUE)
-    for( i in grep( "\\.csv$", filenames ) ) {
-      message( paste("alex: reading", filenames[i] ) )
-      subject.data <- read.csv( filenames[i] )
-      subject.frame <- data.frame( subject.data )
-      pk <- presentation.and.keynum( subject.frame )
-      subject.frame$Presentation <- pk$Presentation
-      subject.frame$KeyNum <- pk$KeyNum
-      data <- rbind( data, subject.frame )
+  require(data.table)
+  dt <- NULL
+  filenames <- dir( paste(data.dir,"/Data",sep=""), full.names=TRUE)
+  for( i in grep( "\\.csv$", filenames ) ) {
+    message( paste("alex: reading", filenames[i] ) )
+    subject.dt <- data.table( read.csv( filenames[i] ) )
+    presentation.and.keynum( subject.dt )
+    dt <- rbind( dt, subject.dt )
+  }
+  ## make sure phase factor is ordered as it appears in the data,
+  ## rather than alphabetically:
+  dt$Phase <- factor( dt$Phase,
+                     levels=unique(as.character(dt$Phase)),
+                     ordered=TRUE )
+  ## but issue a warning if different subjects have different orders:
+  if( "PhaseOrder" %in% names(dt) ) {
+    if( length(unique(dt$PhaseOrder)) > 1 ) {
+      warning( "alex: different subjects have different phase orders:" )
+      warning( "alex: phases cannot be uniquely ordered" )
     }
-    data <- data.frame( data )
-    ## make sure phase factor is ordered as it appears in the data,
-    ## rather than alphabetically:
-    data$Phase <- factor( data$Phase,
-                         levels=unique(as.character(data$Phase)),
-                         ordered=TRUE )
-    ## add a global subject identifier merging group and subject:
-    data$GSubject <- with( data, paste( Group, Subject, sep="." ) )
-    ## correct sex factor (R tends to read "F" as "FALSE"):
-    data$Sex[ data$Sex != "M" ] <- "F"
-    data$Sex <- factor( data$Sex, ordered=FALSE )
-    data
+  }
+  ## add a global subject identifier merging group and subject:
+  dt[ , GSubject := paste( Group, Subject, sep="." ) ]
+  ## correct sex factor (R reads "F" as "FALSE"):
+  dt[ Sex != "M", Sex := "F" ]
+  dt$Sex <- factor( dt$Sex, ordered=FALSE )
+  dt
 }
